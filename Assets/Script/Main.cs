dbeca878510c20a37f3ff67dbde20cd96ba7a663
hiroo <cyariaka0610@gmail.com>
2017-09-21 18:39:56 +0800

    first update

---
+++ Assets/Script/Main.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+    [Header("Setting")]
+    public int trigger_top = 80;
+    public int trigger_buttom = 100;
+
+    [Header("Object")]
+    public GameObject[] canvas;
+
+    [Header("Bool")]
+    public bool showDebugLog = true;
+    public bool door_status = false;
+    
+    private static int val=5000;
+    private static string message;
+    State state;
+    
+
+    // Use this for initialization
+    void Start () {
+        state = State.M1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        StateSwich();
+        DoorStatus();
+
+        switch (state)
+        {
+            case State.M1:
+                canvas[0].SetActive(true);
+                canvas[1].SetActive(false);
+                canvas[2].SetActive(false);
+                break;
+
+            case State.M2:
+                canvas[0].SetActive(false);
+                canvas[1].SetActive(true);
+                canvas[2].SetActive(false);
+                break;
+
+            case State.M3:
+                canvas[0].SetActive(false);
+                canvas[1].SetActive(false);
+                canvas[2].SetActive(true);
+                break;
+
+        }
+
+        if (state == State.M2)
+        {
+            val = getdis_ethernet.value;
+            FoodTrigger(val);
+        }
+            
+
+        
+    }
+    enum State {
+        M1, M2, M3
+    };
+
+    void StateSwich()
+    {
+        if (Input.GetKeyDown(KeyCode.Q)) state = State.M1;
+        if (Input.GetKeyDown(KeyCode.W)) state = State.M2;
+        if (Input.GetKeyDown(KeyCode.E)) state = State.M3;
+    }
+    void DoorStatus()
+    {
+        message = MessageListenerF.message;
+        if (message == "1") {
+            door_status = false;
+            state = State.M1;
+        } 
+        else if (message == "0") door_status = true;
+
+    }
+    void FoodTrigger(int val) {
+        int distance = val ;
+        if (distance > trigger_top && distance < trigger_buttom)
+        {
+            Debug.Log("<color=blue>Get Distance: </color>" + val + "cm!");
+            state = State.M3;
+        }
+    }
+}
---
+++ Assets/SerialComm/fix/MessageListenerF.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * When creating your message listeners you need to implement these two methods:
+ *  - OnMessageArrived
+ *  - OnConnectionEvent
+ */
+
+public class MessageListenerF : MonoBehaviour {
+
+    public bool showDebugLog = true;
+    private bool showOnce = false;
+    public static string message;
+
+    void Update () {
+        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.D))
+        {
+            if (showDebugLog) showDebugLog = false;
+            else showDebugLog = true;
+        }
+    }
+    void OnMessageArrived(string msg)
+    {
+        message = msg;
+        if (!showOnce)
+        {
+            Debug.Log("<color=teal>Serial port message arrived!</color>");
+            showOnce = true;
+        }
+        if (showDebugLog)
+        {
+            Debug.Log(" >>" + msg);
+        }
+    }
+    void OnConnectionEvent(bool success)
+    {
+        if (showDebugLog)
+        {
+        if(success)
+            Debug.Log("<color=green>Connection established.</color>");
+        else
+            Debug.Log("<color=maroon>Connection attempt failed or disconnection detected</color>");
+        }
+            
+    }
+    
+}
