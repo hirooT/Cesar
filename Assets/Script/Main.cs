ce605370091a49a0dd5c6e063e967739a2466add
hiroo <cyariaka0610@gmail.com>
2017-09-27 14:40:23 +0800

    20170926
    update

--- Assets/Script/Main.cs
+++ Assets/Script/Main.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
+using RenderHeads.Media.AVProVideo;
 
 public class Main : MonoBehaviour {
 
@@ -11,65 +12,88 @@ public class Main : MonoBehaviour {
 
     [Header("GameObject")]
     public GameObject[] canvas;
-    public GameObject[] movie_open;
-    public GameObject[] movie_loop;
-    public GameObject[] movie_ending;
-    public GameObject[] movie_other;
+    public GameObject open;
+
+    [Header("Media Player")]
+    public MediaPlayer[] movie_open;
+    public MediaPlayer[] movie_loop;
+    public MediaPlayer[] movie_ending;
+    public MediaPlayer[] movie_other;
+    public MediaPlayer acopy;
 
     [Header("Bool")]
     public bool showDebugLog = true;
     public bool door_status = false;
+    public bool isplaying = false;
 
+    [SerializeField]
     private int index = 0;
     private static int val=5000;
     private static string message;
     State state;
-    
+    State _state;
 
     // Use this for initialization
     void Start () {
         state = State.M1;
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
         StateSwich();
         DoorStatus();
+        StateChange();
 
-        switch (state)
+        if (Input.GetKeyDown(KeyCode.P))
         {
-            case State.M1:
-                canvas[0].SetActive(true);
-                canvas[1].SetActive(false);
-                canvas[2].SetActive(false);
-                break;
-
-            case State.M2:
-                canvas[0].SetActive(false);
-                canvas[1].SetActive(true);
-                canvas[2].SetActive(false);
-                break;
-
-            case State.M3:
-                canvas[0].SetActive(false);
-                canvas[1].SetActive(false);
-                canvas[2].SetActive(true);
-                break;
-
+            if (isplaying) isplaying = false;
+            else isplaying = true;
         }
+        //if (isplaying) acopy.Play();
+        //else if (!isplaying) acopy.Pause();
 
-        if (state == State.M2)
-        {
-            val = getdis_ethernet.value;
-            FoodTrigger(val);
-        }
-            
-
+        //Debug.Log((int)acopy.Control.GetCurrentTimeMs()/1000);
         
     }
     enum State {
         M1, M2, M3
     };
+
+    public void StateChange()
+    {
+            switch (state)
+            {
+                case State.M1:
+                    open.GetComponent<DisplayUGUI>()._mediaPlayer = movie_open[index];
+                    canvas[0].SetActive(true);
+                    canvas[1].SetActive(false);
+                    canvas[2].SetActive(false);
+                    //alphaInit();
+                    break;
+
+                case State.M2:
+                    canvas[0].SetActive(false);
+                    canvas[1].SetActive(true);
+                    canvas[2].SetActive(false);
+                    val = getdis_ethernet.value;
+                    FoodTrigger(val);
+                    break;
+
+                case State.M3:
+                    canvas[0].SetActive(false);
+                    canvas[1].SetActive(false);
+                    canvas[2].SetActive(true);
+                    break;
+
+            }
+    }
+
+    void alphaInit()
+    {
+        //canvas alpha init
+        //M1:0 M2:1 M3-1:0 M3-2:0
+    }
 
     void StateSwich()
     {
@@ -80,16 +104,28 @@ public class Main : MonoBehaviour {
 
     void DoorStatus()
     {
-        message = MessageListenerF.message;
-        if (message == "1")
+        if(message != MessageListenerF.message)
         {
-            door_status = false;
-            state = State.M1;
+            if (message == "0")
+            {
+                door_status = false;
+                state = State.M1;
+                if (index < 2) index++;
+                else index = 0;
+                //movie_open[index].Pause();
+                movie_open[index].Rewind(true);
+                Debug.Log("door close.");
+            }
+            else if (message == "1")
+            {
+                door_status = true;
+                movie_open[index].Play();
+                Debug.Log("door open.");
+            }
         }
-        else if (message == "0") {
-            door_status = true;
-        }
-
+        message = MessageListenerF.message;
+        
+        //Debug.Log(message);
     }
     void FoodTrigger(int val) {
         int distance = val ;
@@ -119,4 +155,6 @@ public class Main : MonoBehaviour {
             ending.Add(new Movie(movie_ending[i].name, i));
         }
     }
+
+
 }
